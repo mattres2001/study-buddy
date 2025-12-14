3fadc0c8a04c651b86533f8bc2443220165994aa
Matthieu Resurreccion <mattres2001@Matthieus-MacBook-Air-2.local>
2025-12-14 12:24:40 -0800

    Added story viewing functions

--- src/client/src/components/StoriesBar.jsx
+++ src/client/src/components/StoriesBar.jsx
@@ -3,6 +3,7 @@ import { dummyStoriesData } from '../assets/assets'
 import { Plus } from 'lucide-react'
 import moment from 'moment'
 import StoryModal from './StoryModal'
+import StoryViewer from './StoryViewer'
 
 const StoriesBar = () => {
 
@@ -36,7 +37,7 @@ const StoriesBar = () => {
             {/* Story Cards */}
             {
                 stories.map((story, index) => (
-                    <div key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40
+                    <div onClick={() => setViewStory(story)} key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40
                         cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b 
                         from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 
                         active:scale-95`}>
@@ -71,7 +72,8 @@ const StoriesBar = () => {
         
         {/* Add Stories Modal */}
         {showModal && <StoryModal setShowModal={setShowModal} fetchStories={fetchStories} />}
-
+        {/* View Story Modal */}
+        {viewStory && <StoryViewer viewStory={viewStory} setViewStory={setViewStory} />}
     </div>
     )
 }
