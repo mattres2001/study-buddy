89da9ddc3c7a90282f52f5c1aa27f0ba1a7b4e0f
Matthieu Resurreccion <mattres2001@Matthieus-MacBook-Air-2.local>
2026-01-06 16:14:50 -0800

    Created Edit Profile page

--- src/client/src/pages/Profile.jsx
+++ src/client/src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import UserProfileInfo from '../components/UserProfileInfo'
 import PostCard from '../components/PostCard'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import ProfileModal from '../components/ProfileModal'
 
 
 const Profile = () => {
@@ -77,7 +78,7 @@ const Profile = () => {
                 </div>
             </div>
             {/* Edit Profile Modal */}
-            {showEdit && <p>show profile edit</p>}
+            {showEdit && <ProfileModal setShowEdit={setShowEdit}/>}
         </div>
     ) : (<Loading />)
 }
