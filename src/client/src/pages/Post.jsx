35b5ef6417ac0d45487670f8eba56857b600f6f6
Matthieu Resurreccion <mattres2001@Matthieus-MacBook-Air-4.local>
2026-02-05 14:27:38 -0800

    working on post page

--- src/client/src/pages/Post.jsx
+++ src/client/src/pages/Post.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../components/Loading'
+import PostCard from '../components/PostCard'
+import api from '../api/axios'
 
 const Post = () => {
-    return (
+
+    const [ post, setPost ] = useState(null)
+    const [ loading, setLoading ] = useState(true)
+    const { getToken } = useAuth()
+
+    const fetchPost = async () => {
+        try {
+            const { data } = await api.get('/api/post/view', { headers: { Authorization: `Bearer ${await getToken()}` }})
+            if (data.success)
+                setPost(data);
+            else
+                toast.error(data.message)
+            setLoading(true)
+        } catch (error) {
+            toast.error(error.message)
+        }
+        setLoading(false)
+    }
+
+    return !loading ? (
         <div>
             
         </div>
-    )
+    ) : <Loading/>
 }
 
 export default Post
