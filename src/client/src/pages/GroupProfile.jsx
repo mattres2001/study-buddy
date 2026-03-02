9b1fc823a2713f45c02d6b0da71a3187b8977c08
LosAngielez <angie@Mac.lan>
2026-03-01 22:00:52 -0800

    media

--- src/client/src/pages/GroupProfile.jsx
+++ src/client/src/pages/GroupProfile.jsx
@@ -23,15 +23,47 @@ const GroupCard = () => {
           <div className= 'mt-6'>
             <div className= 'bg-white rounded-xl shadow p-1 flex max-w-md mx-auto'>
                 {["posts", "media", "likes"].map((tab)=>(
-                    <button key={tab} className='flex-1 px-4 py-2 text-sm font-medium
+                    <button onClick={()=> setActiveTab(tab)} key={tab} className='flex-1 px-4 py-2 text-sm font-medium
                     rounded-lg transition-colors cursor-pointer ${activeTab === tab ?
                     "bg-indigo-600 text-white" : "text-gray-600 hover:text-gray-900"}'}>
                     {tab.chartAT(0).toUpperCase() + tab.slice(1)}
                     </button>
                 ))}
             </div>
+            {/* posts */}
+            {activeTab === 'posts'&& (
+                <div className= 'mt-6 flex flex-col items-center gap-6'>
+                    {posts.map((post)=> <PostCard  key={post._id} post={post}/>)}
+                <div> 
+            )}
+
+            {/*media*/}
+                {activeTab === 'media' && (
+                    <div className= 'flex flex-wrap mt-6 max-w-6xl'>
+                        {
+                        posts.filter((post)=>post.image_urls.length > 0).map((post)=>(
+                            <>
+                            {post.image_urls.map((image, index)=>(
+                                <Link target='_blank' to={image} key={index}
+                                className='relative group'>
+                                <img src={image} key={index} className= 'w-64' aspect-video
+                                object-cover' alt= "" />
+                                <p className= 'absolute bottom-0 right-0 text-xs p-1 px-3 
+                                backdrop-blur-xl text-white opacity-0 group-hover:opacity-100
+                                transition duration-300'>Posted {moment(post.createdAt).
+                                fromNow()}</p>
+                                </Link>
+                             ))}
+                          </>
+                         ))
+                        }
+                    <div>
+                  )}
+
           </div>
-         </div>            
+         </div> 
+         {/* Edit Profile Modal */}
+         {showEdit && <p> show profile edit </p>}           
         </div>
     ) : (<Loading />)
 }
